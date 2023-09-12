36112cfd6a6c6d4b2eacad2c2c663979fd244d4c
AirZED <regnalukpabio@gmail.com>
2023-09-12 09:03:11 +0100

    biilding questions model

--- src/models/questionModel.ts
+++ src/models/questionModel.ts
@@ -1,12 +1,28 @@
 import mongoose, { Document } from 'mongoose';
+import validator from 'validator';
 
-const questionSchema = new mongoose.Schema(
+export interface IQuestion extends Document {
+  question: String;
+  options: String[] | number[] | boolean[];
+  answer: String | number | boolean;
+}
+
+const questionSchema = new mongoose.Schema<IQuestion>(
   {
     question: {
       type: String,
       required: [true, 'Question must have a question'],
     },
-    answers: { type: Array, required: [true, 'Question must have answers'] },
+    options: {
+      type: [{ type: String || Number, trim: true }],
+      validate: {
+        validator: function (this: IQuestion, options: any): boolean {
+          return options.length <= 4;
+        },
+        message: 'Options must not be more than 4',
+      },
+      required: [true, 'Question must have options'],
+    },
   },
   {
     timestamps: true,
